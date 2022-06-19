2d2a8e5c5f519735b2c0b887a5482b1aed8ff213
nattyKhoe <natasha.kusuma@outlook.com>
2022-06-19 18:28:43 +1000

    commit tree 0927186147d472f936c313bf7e32177ff5cd20b5

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick');
+window.onload= function(){
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+let dogBreed = document.querySelector("#dog-breeds");
+
+fetch(imgUrl)
+.then( response => response.json())
+.then( json => {
+    json.message.forEach(dog => {
+        let dogImg = document.createElement("img");
+        dogImg.src = dog;
+        dogImg.style.height = '200px';
+        document.querySelector("#dog-image-container").appendChild(dogImg); //or store "dog-image-container" into a variable
+    });
+});
+
+fetch(breedUrl)
+.then( response => response.json() )
+.then( json=> {
+    for (let breed in json.message){
+        let breedLI = document.createElement("li");
+        breedLI.innerText = breed;
+        
+        dogBreed.appendChild(breedLI);
+    };
+});
+
+dogBreed.addEventListener ('click', function(e){
+    if(e.target.tagName === "LI"){
+        e.target.style.color = 'red';
+    }
+});
+
+document.querySelector("#breed-dropdown").addEventListener('change', function (e){
+    dogBreed.innerHTML ="";
+    let option = document.querySelector("#breed-dropdown").value;
+
+    fetch(breedUrl)
+    .then( response => response.json())
+    .then( json =>{
+        for (let breed in json.message) {
+            if (breed[0] === option){
+                let breedLI = document.createElement("li");
+                breedLI.innerText = breed;
+                
+                dogBreed.appendChild(breedLI);
+            }
+        }
+    });
+    });
+};
+
